d60ffb5bf62800bfa6f548f419717101c58ed38e
Alysson Henrique <alyssonhra99@gmail.com>
2023-04-04 11:55:14 -0300

    working with bug (it's possible to click in more than 2 cards before it rotates back

--- script.js
+++ script.js
@@ -1,29 +1,68 @@
 const container = document.querySelector(".container");
 const gifsCards = [
-    "./Arquivos Úteis - Projeto 04 - Parrot Card Game/bobrossparrot.gif",
-    "./Arquivos Úteis - Projeto 04 - Parrot Card Game/explodyparrot.gif",
-    "./Arquivos Úteis - Projeto 04 - Parrot Card Game/fiestaparrot.gif",
-    "./Arquivos Úteis - Projeto 04 - Parrot Card Game/metalparrot.gif",
-    "./Arquivos Úteis - Projeto 04 - Parrot Card Game/revertitparrot.gif",
-    "./Arquivos Úteis - Projeto 04 - Parrot Card Game/tripletsparrot.gif",
-    "./Arquivos Úteis - Projeto 04 - Parrot Card Game/unicornparrot.gif",
-  ];
+  "./Arquivos Úteis - Projeto 04 - Parrot Card Game/bobrossparrot.gif",
+  "./Arquivos Úteis - Projeto 04 - Parrot Card Game/explodyparrot.gif",
+  "./Arquivos Úteis - Projeto 04 - Parrot Card Game/fiestaparrot.gif",
+  "./Arquivos Úteis - Projeto 04 - Parrot Card Game/metalparrot.gif",
+  "./Arquivos Úteis - Projeto 04 - Parrot Card Game/revertitparrot.gif",
+  "./Arquivos Úteis - Projeto 04 - Parrot Card Game/tripletsparrot.gif",
+  "./Arquivos Úteis - Projeto 04 - Parrot Card Game/unicornparrot.gif",
+];
 
 let qtde = 0;
 let nCards = [];
 
+function isTheSameCard(firstChoice, currentOne) {
+  
+  currentOne.classList.add("rotate-back");
+
+  function rotateCardsBack() {
+    
+    firstChoice.classList.remove("rotate-back"); 
+    currentOne.classList.remove("rotate-back");
+    
+  }
+
+  const card1 = firstChoice.querySelector('.gif').src;
+  const card2 = currentOne.querySelector('.gif').src;
+
+  if (card1 !== card2) {
+    setTimeout(       
+      () => rotateCardsBack()     
+    , 1000)
+  } else {
+    firstChoice.classList.remove("not-found"); 
+    currentOne.classList.remove("not-found");
+  }
+  
+};
+
+function rotateCard(card) {
+
+  const firstChoice = container.querySelector('.rotate-back.not-found');
+
+  if (firstChoice !== null) {
+    isTheSameCard(firstChoice, card.querySelector('.back-face'));
+
+  } else {
+    const backFace = card.querySelector(".back-face");    
+    
+    backFace.classList.add("rotate-back");
+  }
+};
+
 function comparador() {
   return Math.random() - 0.5;
-}
+};
 
 const card = (randomImg) => {
   return `
-            <div class="card">
+            <div class="card" onclick="rotateCard(this)">
                 <div class="front-face face">
                     <img src="./Arquivos Úteis - Projeto 04 - Parrot Card Game/back.png" alt="back-img" />
                 </div>
-                <div class="back-face face">
-                    <img src="${randomImg}" alt="img" />
+                <div class="back-face face not-found">
+                    <img class="gif" src="${randomImg}" alt="img" />
                 </div>
             </div>
         `;
@@ -31,21 +70,15 @@ const card = (randomImg) => {
 
 while (qtde < 4 || qtde > 14 || qtde % 2 !== 0) {
   qtde = prompt("Com quantas cartas você deseja jogar?");
-}
+};
 
-for (let i = 0; i < qtde/2; i++) {
-  console.log(gifsCards.sort(comparador)[i]);
-  nCards.push(card(gifsCards.sort(comparador)[i]));
-}
-console.log(nCards)
+const mixedCards = gifsCards.sort(comparador);
+
+for (let i = 0; i < qtde / 2; i++) {
+  nCards.push(card(mixedCards[i]));
+};
+
 nCards = [...nCards, ...nCards];
 nCards = nCards.sort(comparador);
-console.log(nCards)
-container.innerHTML = nCards.join('');
 
-const cardsGame = document.getElementsByClassName("card");
-for (let j = 0; j <  cardsGame.length; j++) {
-    cardsGame[j].addEventListener("click", (ev) => {
-        ev.currentTarget.querySelector('.back-face').classList.add("rotate");        
-    });
-};
+container.innerHTML = nCards.join("");
